b00ad73b8297af6df3b7d555054417665ce6fb14
Annu Bhagat <AnnuB@quinnox.com>
2019-01-30 19:48:45 +0530

    Add project files.

---
+++ WebhookTest/Controllers/MessagingController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Mvc;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.TwiML;
+
+namespace WebhookTest.Controllers
+{
+    public class MessagingController : TwilioController
+    {
+        [HttpPost]
+        public TwiMLResult Index()
+        {
+            var response = new MessagingResponse();
+            response.Message("Welcome!!!");
+            return TwiML(response);
+        }
+    }
+}
